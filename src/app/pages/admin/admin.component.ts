b276c9477d7d24da0e1396761c3b518108dab7a9
Sofi Brugo <brugosofi@gmail.com>
2024-06-26 00:00:42 -0300

    admin page/mp

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -38,28 +38,7 @@ export class AdminComponent {
   }
 
 
-  transformDriveUrl(url: string): string {
-    const drivePrefix = 'https://drive.google.com/file/d/';
-    const driveSuffix = '/view?usp=sharing';
-    const transformedPrefix = 'https://drive.google.com/uc?export=view&id=';
 
-  
-    if (url.startsWith(transformedPrefix)) {
-      console.log("Already a transformed Drive URL, returning original:", url);
-      return url;
-    }
-
-    // Verificar si la URL es de Google Drive
-    if (url.startsWith(drivePrefix) && url.includes(driveSuffix)) {
-      const fileIdStart = url.indexOf(drivePrefix) + drivePrefix.length;
-      const fileIdEnd = url.indexOf(driveSuffix);
-      const fileId = url.substring(fileIdStart, fileIdEnd);
-      return `https://drive.google.com/uc?export=view&id=${fileId}`;
-    }
-    console.log("Not a Drive URL, returning original:", url); // Log non-Drive URLs
-
-    return url;
-  }
   submitForm() {
 
     this.raffle.name = "SORTEO ACTUAL";
@@ -68,24 +47,13 @@ export class AdminComponent {
       this.message="Debes ingresar todos los campos";
       return;
     }
-    console.log("Before transformation:", this.raffle.urlImage1, this.raffle.urlImage2, this.raffle.urlImage3);
-
-    this.raffle.urlImage1 = this.transformDriveUrl(this.raffle.urlImage1);
-    this.raffle.urlImage2 = this.transformDriveUrl(this.raffle.urlImage2);
-    this.raffle.urlImage3 = this.transformDriveUrl(this.raffle.urlImage3);
-    
-    console.log("After transformation:", this.raffle.urlImage1, this.raffle.urlImage2, this.raffle.urlImage3);
-
     this.adminService.addRaffle(this.raffle).subscribe(() => {
-/*      alert("Sorteo agregado correctamente");*/
       this.message = "Sorteo agregado correctamente!";
     }, err => {
       if (err.status === 400)
-/*        alert("Ya existe un sorteo activo");*/
       this.message = "Ya existe un sorteo activo.";
 
       else
-/*        alert("Ocurrió un error...");*/
 this.message = "Ocurrió un error...";
 
     });
@@ -93,11 +61,9 @@ this.message = "Ocurrió un error...";
 
   deleteRaffle(){
     this.adminService.deleteRaffle().subscribe(()=>{
-/*      alert("Sorteo eliminado con exito");*/
-this.message= "Sorteo eliminado con exito";
+    this.message= "Sorteo eliminado con exito";
     },err=>{
-/*      alert("No hay un sorteo activo")*/
-this.message= "Hola! No hay un sorteo activo.";
+    this.message= "No hay un sorteo activo.";
     });
 
   }
